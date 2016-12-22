c543de2f994d5c9aff884894d52cc2fcac34570b
Leon Do <leondo@Leons-MacBook-Pro.local>
2016-12-22 14:05:57 -0500

    removed ebay stuff

--- ebay.js
+++
@@ -1,9 +0,0 @@
-var casper = require('casper').create();
-var x = require('casper').selectXPath;
-var fs = require('fs');
-var request = require('request')
-
-
-request('file:///Users/leondo/Desktop/jet.com-scraper/jetData.html', function (error, response, body) {
-  console.log(body)
-})
